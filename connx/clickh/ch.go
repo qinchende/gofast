a6aca2a5c6bf593298fe14ed871bdff0122299e2
root <root@chende-pc.localdomain>
2021-03-26 01:13:27 +0800

    clickhouse

---
+++ connx/clickh/ch.go
@@ -0,0 +1,29 @@
+package clickh
+
+import (
+	"context"
+	"database/sql"
+	"log"
+)
+
+type (
+	ConnConfig struct {
+		ConnStr string `json:",optional"`
+	}
+	ClickHouseX struct {
+		Cli *sql.DB
+		Ctx context.Context
+	}
+)
+
+func NewClickH(cf *ConnConfig) *ClickHouseX {
+	chX := ClickHouseX{Ctx: context.Background()}
+
+	conn, err := sql.Open("clickhouse", cf.ConnStr)
+	if err != nil {
+		log.Fatalf("Conn %s err: %s", cf.ConnStr, err)
+	}
+
+	chX.Cli = conn
+	return &chX
+}
