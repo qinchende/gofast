84ee5cab3cf3f7a6e11601afe8a0d59e6fabcf3e
root <qinchende@qq.com>
2021-11-18 22:50:40 +0800

    breaker

--- fst/check/req_counter.go
+++ fst/check/req_counter.go
@@ -8,12 +8,12 @@ import (
 )
 
 type (
-	FuncGetPath func(id int16) string
+	FuncGetPath func(id int16) string // 获取当前请求对应的路径
 
 	ReqItem struct {
-		Duration  time.Duration // 任务耗时
-		RouterIdx int16         // 路由节点的index
-		Drop      bool          // 是否是一个丢弃的任务
+		RouterIdx int16         // 当前请求对应路由树节点的index
+		Duration  time.Duration // 请求耗时
+		Drop      bool          // 是否是一个被丢弃的请求（这样好统计服务器的压力，单单只是出错不能算后台服务压力大吧？）
 	}
 
 	reqItems struct {
@@ -26,12 +26,13 @@ type (
 		getPath FuncGetPath
 		//name     string
 		pid      int
-		items    []ReqItem
 		duration time.Duration
+		items    []ReqItem
 		drops    int
 	}
 )
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // 添加统计项目
 func (rc *reqContainer) AddItem(v interface{}) bool {
 	if item, ok := v.(ReqItem); ok {
@@ -48,8 +49,8 @@ func (rc *reqContainer) AddItem(v interface{}) bool {
 // 执行
 func (rc *reqContainer) Execute(items interface{}) {
 	ret := items.(reqItems)
-	//items := pair.items
-	//duration := pair.duration
+	// items := pair.items
+	// duration := pair.duration
 	drops := ret.drops
 	size := len(ret.items)
 	report := &PrintInfo{
@@ -60,14 +61,14 @@ func (rc *reqContainer) Execute(items interface{}) {
 		Drops:         drops,
 	}
 	if size > 0 {
-		//report.PerDur = (ret.duration / time.Millisecond) / size
+		// report.PerDur = (ret.duration / time.Millisecond) / size
 		report.Path = rc.getPath(ret.items[0].RouterIdx)
 	}
 
 	log(report)
 }
 
-// 返回当前容器中的所有数据
+// 返回当前容器中的所有数据，同时清空容器
 func (rc *reqContainer) RemoveAll() interface{} {
 	items := rc.items
 	duration := rc.duration
