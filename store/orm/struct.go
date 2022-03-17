1298fec98af753db6199eb1e42684f0f1c992909
闪电侠 <qinchende@qq.com>
2022-03-17 22:38:17 +0800

    orm

--- store/orm/struct.go
+++ store/orm/struct.go
@@ -1,8 +1,9 @@
 package orm
 
 import (
-	"fmt"
+	"github.com/qinchende/gofast/logx"
 	"reflect"
+	"time"
 )
 
 const (
@@ -10,106 +11,106 @@ const (
 	dbTagOther = "pms"
 )
 
-// 结构体中属性的数据库字段名称合集
-func DbFieldNames(in interface{}, includes ...bool) []string {
-	out := make([]string, 0)
-	val := reflect.ValueOf(in)
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	// we only accept structs
-	if val.Kind() != reflect.Struct {
-		panic(fmt.Errorf("ToMap only accepts structs; got %T", val))
+func DbFieldValues2(ins ...interface{}) ([]string, []interface{}) {
+	fls := make([]string, 0)
+	values := make([]interface{}, 0)
+
+	for i := 0; i < len(ins); i++ {
+		tF, tV := DbFieldValues(ins[i])
+
+		fls = append(fls, tF...)
+		values = append(values, tV...)
 	}
 
-	typ := val.Type()
-	for i := 0; i < val.NumField(); i++ {
-		// gets us a StructField
+	return fls, values
+}
+
+// 结构体中属性的数据库字段名称合集
+func DbFieldValues(in interface{}, includes ...bool) ([]string, []interface{}) {
+	fls := make([]string, 0)
+	values := make([]interface{}, 0)
+
+	sVal := reflect.ValueOf(in)
+	if sVal.Kind() == reflect.Ptr {
+		sVal = sVal.Elem()
+	}
+
+	// we only accept structs
+	if sVal.Kind() != reflect.Struct {
+		//panic(fmt.Errorf("ToMap only accepts structs; got %T", sVal))
+		s, v := singleField(sVal.Interface())
+		fls = append(fls, s)
+		values = append(values, v)
+
+		return fls, values
+	}
+
+	if _, ok := sVal.Interface().(time.Time); ok {
+		s, v := singleField(sVal.Interface())
+		fls = append(fls, s)
+		values = append(values, v)
+		return fls, values
+	}
+
+	typ := sVal.Type()
+	for i := 0; i < sVal.NumField(); i++ {
+		// value: get value
+		va := sVal.Field(i)
+		if va.Kind() == reflect.Struct {
+			vaInter := va.Interface()
+			if _, ok := vaInter.(time.Time); !ok {
+				subFields, subValues := DbFieldValues(vaInter)
+				fls = append(fls, subFields...)
+				values = append(values, subValues...)
+				continue
+			}
+		}
+		values = append(values, va.Interface())
+
+		// type: gets us a StructField
 		fi := typ.Field(i)
 		dbf := fi.Tag.Get(dbTag)
 		if dbf == "" {
 			dbf = fi.Tag.Get(dbTagOther)
 		}
 		if dbf != "" {
-			out = append(out, dbf)
+			fls = append(fls, dbf)
 		} else {
-			out = append(out, fi.Name)
+			fls = append(fls, fi.Name)
 		}
 	}
+	//var includeId bool
+	//if len(includes) > 0 {
+	//	includeId = includes[0]
+	//}
 
-	return out
+	return fls, values
 }
 
-//
-//// ToMap converts interface into map
-//func ToMap(in interface{}) map[string]interface{} {
-//	out := make(map[string]interface{})
-//	v := reflect.ValueOf(in)
-//	if v.Kind() == reflect.Ptr {
-//		v = v.Elem()
-//	}
-//
-//	// we only accept structs
-//	if v.Kind() != reflect.Struct {
-//		panic(fmt.Errorf("ToMap only accepts structs; got %T", v))
-//	}
-//
-//	typ := v.Type()
-//	for i := 0; i < v.NumField(); i++ {
-//		// gets us a StructField
-//		fi := typ.Field(i)
-//		if tagv := fi.Tag.Get(dbTag); tagv != "" {
-//			// set key of map to value in struct field
-//			val := v.Field(i)
-//			zero := reflect.Zero(val.Type()).Interface()
-//			current := val.Interface()
-//
-//			if reflect.DeepEqual(current, zero) {
-//				continue
-//			}
-//			out[tagv] = current
-//		}
-//	}
-//
-//	return out
-//}
+func singleField(in interface{}) (string, interface{}) {
+	typ := reflect.TypeOf(in)
+	sVal := reflect.ValueOf(in)
 
-//// RawFieldNames converts golang struct field into slice string
-//func RawFieldNames(in interface{}, postgresSql ...bool) []string {
-//	out := make([]string, 0)
-//	v := reflect.ValueOf(in)
-//	if v.Kind() == reflect.Ptr {
-//		v = v.Elem()
-//	}
-//
-//	var pg bool
-//	if len(postgresSql) > 0 {
-//		pg = postgresSql[0]
-//	}
-//
-//	// we only accept structs
-//	if v.Kind() != reflect.Struct {
-//		panic(fmt.Errorf("ToMap only accepts structs; got %T", v))
-//	}
-//
-//	typ := v.Type()
-//	for i := 0; i < v.NumField(); i++ {
-//		// gets us a StructField
-//		fi := typ.Field(i)
-//		if tagv := fi.Tag.Get(dbTag); tagv != "" {
-//			if pg {
-//				out = append(out, fmt.Sprintf("%s", tagv))
-//			} else {
-//				out = append(out, fmt.Sprintf("`%s`", tagv))
-//			}
-//		} else {
-//			if pg {
-//				out = append(out, fmt.Sprintf("%s", fi.Name))
-//			} else {
-//				out = append(out, fmt.Sprintf("`%s`", fi.Name))
-//			}
-//		}
-//	}
-//
-//	return out
-//}
+	logx.Info(typ)
+	logx.Info(sVal)
+
+	//sVal
+	//
+	//values = append(values, va.Interface())
+	//
+	//typ := sVal.Type()
+	//
+	//// type: gets us a StructField
+	//fi := typ.Field(i)
+	//dbf := fi.Tag.Get(dbTag)
+	//if dbf == "" {
+	//	dbf = fi.Tag.Get(dbTagOther)
+	//}
+	//if dbf != "" {
+	//	fls = append(fls, dbf)
+	//} else {
+	//	fls = append(fls, fi.Name)
+	//}
+
+	return "", ""
+}
