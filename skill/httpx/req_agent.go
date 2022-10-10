a80a8f8e484dfe9a6a60048b81d3262b0ec6cfdb
闪电侠 <qinchende@qq.com>
2022-10-10 22:35:34 +0800

    request pet

--- skill/httpx/req_agent.go
+++ skill/httpx/req_agent.go
@@ -1,1 +1,29 @@
 package httpx
+
+import (
+	"github.com/qinchende/gofast/logx"
+	"net/http"
+	"net/url"
+)
+
+func getTransport(proxyUrl string) *http.Transport {
+	if proxyUrl == "" {
+		return nil
+	}
+
+	if myTransports[proxyUrl] != nil {
+		return myTransports[proxyUrl]
+	}
+
+	netURL, err := url.Parse(proxyUrl)
+	if err != nil {
+		logx.Debugs(err)
+		return nil
+	}
+
+	trans := &http.Transport{
+		Proxy: http.ProxyURL(netURL),
+	}
+	myTransports[proxyUrl] = trans
+	return trans
+}
