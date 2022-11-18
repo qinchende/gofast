d7646474aa85b205c7f869f973ee29d346f0485b
闪电侠 <qinchende@qq.com>
2022-11-19 01:23:51 +0800

    breaker

--- sdx/midware.go
+++ sdx/midware.go
@@ -14,7 +14,7 @@ func SuperHandlers(app *fst.GoFast) *fst.GoFast {
 	cnf := app.SdxConfig
 
 	// 初始化一个全局的 请求管理器（记录访问数据，分析统计，限流降载熔断，定时日志）
-	reqKeeper := gate.NewReqKeeper(app.ProjectName())
+	keeper := gate.NewReqKeeper(app.ProjectName())
 	app.OnBeforeBuildRoutes(func(app *fst.GoFast) {
 		// 因为Routes的数量只能在加载完所有路由之后才知道,所以这里选择延时构造所有Breakers
 		mid.AllAttrs.Rebuild(app.RoutesLen(), &cnf) // 所有路由配置
@@ -22,7 +22,7 @@ func SuperHandlers(app *fst.GoFast) *fst.GoFast {
 
 		routePaths := app.RoutePathsWithMethod()
 		extraPaths := []string{"AllRequest", "RouteMatched", "LoadShedding"}
-		reqKeeper.InitAndRun(routePaths, extraPaths) // 看守上岗
+		keeper.InitAndRun(routePaths, extraPaths) // 看守上岗
 	})
 
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -30,33 +30,29 @@ func SuperHandlers(app *fst.GoFast) *fst.GoFast {
 	// NOTE：Fit系列是全局的，针对所有请求起作用，而且不区分路由，这个时候还根本没有开始匹配路由。
 	// GoFast提供默认的全套拦截器，开启微服务治理
 	// 请求按照先后顺序依次执行这些拦截器，顺序不可随意改变
-	app.UseHttpHandler(mid.HttpReqCountPos(reqKeeper, 0))              // 访问计数1
+	app.UseHttpHandler(mid.HttpReqCountPos(keeper, 0))                 // 访问计数1
 	app.UseHttpHandler(mid.HttpMaxConnections(cnf.MaxConnections))     // 最大同时处理请求数量
 	app.UseHttpHandler(mid.HttpMaxContentLength(cnf.MaxContentLength)) // 请求头最大限制
 
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	// 第二级：ContextHandlers 带上下文 fst.Context 的执行链
-	app.Before(mid.ReqCountPos(reqKeeper, 1))             // 正确匹配路由的请求数
-	app.Before(mid.Tracing(cnf.EnableTrack))              // 链路追踪
-	app.Before(mid.Logger)                                // 请求日志
-	app.Before(mid.Breaker(reqKeeper))                    // 自适应熔断
-	app.Before(mid.LoadShedding(reqKeeper, 2))            // 过载保护，同时也提升了熔断比率
-	app.Before(mid.Timeout(reqKeeper, cnf.EnableTimeout)) // 超时自动返回（请求在后台任然继续执行）
-	app.Before(mid.Recovery)                              // @@@ 截获所有异常，避免服务进程崩溃 @@@
-	app.Before(mid.TimeMetric(reqKeeper))                 // 耗时统计
-	app.Before(mid.MaxContentLength)                      // 分路由判断请求长度
-	app.Before(mid.Gunzip)                                // 自动 gunzip 解压缩
-
-	// 下面的这些特性恐怕都需要用到 fork 时间模式添加监控。
-	//app.Before(mid.Prometheus)                 // 适合 prometheus 的统计信息
-	// app.Fit(mid.JwtAuthorize(app.FitJwtSecret))
+	app.Before(mid.ReqCountPos(keeper, 1))             // 正确匹配路由的请求数
+	app.Before(mid.Tracing(cnf.EnableTrack))           // 链路追踪
+	app.Before(mid.Logger)                             // 请求日志
+	app.Before(mid.Breaker(keeper))                    // 自适应熔断
+	app.Before(mid.LoadShedding(keeper, 2))            // 过载保护，同时也提升了熔断比率
+	app.Before(mid.Timeout(keeper, cnf.EnableTimeout)) // 超时自动返回（请求在后台任然继续执行）
+	app.Before(mid.Recovery)                           // @@@ 截获所有异常，避免服务进程崩溃 @@@
+	app.Before(mid.TimeMetric(keeper))                 // 耗时统计
+	app.Before(mid.MaxContentLength)                   // 分路由判断请求长度
+	app.Before(mid.Gunzip)                             // 自动 gunzip 解压缩
 
 	// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	// 特殊路由的处理链
 	// 正确匹配路由之外的情况，比如特殊的404,504等路由处理链
-	if cnf.UseSpecialHandlers {
-		app.SpecialBefore(mid.ReqCount(reqKeeper)) // 数量统计
-		app.SpecialBefore(mid.LoggerMini)          // 特殊路径的日志
+	if cnf.EnableSpecialHandlers {
+		app.SpecialBefore(mid.ReqCount(keeper)) // 数量统计
+		app.SpecialBefore(mid.LoggerMini)       // 特殊路径的日志
 	}
 	return app
 }
