59a83da83c87d4c0ba924d8ea441ce4eb13a5895
闪电侠 <qinchende@qq.com>
2023-04-08 23:29:14 +0800

    json to gson

--- skill/jsonx/decode_brackets.go
+++ skill/jsonx/decode_brackets.go
@@ -1,20 +1,37 @@
 package jsonx
 
-// 大括号
-type bracesMark struct {
-	left  []uint32
-	right []uint32
-	lIdx  uint32
-	rIdx  uint32
-}
+//type fastDecode struct {
+//	dst  cst.SuperKV
+//	gr   *gson.GsonRow // Gson 作为特殊解析对象，单独处理
+//	src  string
+//	head uint32
+//	tail uint32
+//
+//	// 这里的内存分配不是在栈上，因为后面要用到，发生了逃逸。既然已经逃逸，可以考虑动态初始化
+//	// 即使逃逸也有一定意义，同一次解析中共享了内存
+//	share []byte
+//	//braces  bracesMark  // 大括号
+//	//squares squaresMark // 中括号
+//
+//	// 解析过程中用到临时变量
+//	seg segment // 当前解析中的片段
+//}
 
-// 中括号，方括号
-type squaresMark struct {
-	left  []uint32
-	right []uint32
-	lIdx  uint32
-	rIdx  uint32
-}
+//// 大括号
+//type bracesMark struct {
+//	left  []uint32
+//	right []uint32
+//	lIdx  uint32
+//	rIdx  uint32
+//}
+//
+//// 中括号，方括号
+//type squaresMark struct {
+//	left  []uint32
+//	right []uint32
+//	lIdx  uint32
+//	rIdx  uint32
+//}
 
 //// 先扫描字符串，获取所有左右括号的位置信息
 //func (dd *fastDecode) searchBrackets() error {
